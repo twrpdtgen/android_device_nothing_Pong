366831f49543c65c03e2244c08a891af84fa8d53
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-12-19 02:55:39 +0100

    Pong: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: nothing Model: A065 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Pong.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_Pong-user \
+    omni_Pong-userdebug \
+    omni_Pong-eng
